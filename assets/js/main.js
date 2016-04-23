42c7ea9860695ac3b8e0595395c3475e3d59f800
williwambu <willi.wambu@gmail.com>
2016-04-23 18:54:44 +0300

    Added portfolio section

--- assets/js/main.js
+++ assets/js/main.js
@@ -56,6 +56,18 @@ jQuery(document).ready(function( $ ) {
        $('#menu').not(this).removeClass("active");
        $(this).toggleClass("active");
       });
+      
+      //Portfolio
+          $("[rel='tooltip']").tooltip();    
+ 
+          $('.thumbnail').hover(
+              function(){
+                  $(this).find('.caption').slideDown(250); //.fadeIn(250)
+              },
+              function(){
+                  $(this).find('.caption').slideUp(250); //.fadeOut(205)
+              }
+          ); 
 
   });
 
